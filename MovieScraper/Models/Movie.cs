4b5932761c2bcb5d94249b9bd0d3115af1f9f652
Paul Willis <107751076+JustPaulWillis@users.noreply.github.com>
2022-08-25 00:54:37 -0700

    Initial Commit

---
+++ MovieScraper/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieScraper
+{
+    internal class Movie
+    {
+        public string MovieURL;
+        //rating
+        //other metadata
+
+        public Movie(string MovieURL)
+        {
+            this.MovieURL = MovieURL;
+           
+        }
+    }
+}
